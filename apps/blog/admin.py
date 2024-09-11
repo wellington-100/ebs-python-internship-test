45f637eab2de904ccd960aa12a1a6854eb26e6f5
Wellington-100 <vasile.andruscenco@gmail.com>
2024-09-11 22:10:14 +0300

    milestone_1|task_2|addind in blog list name and status

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from apps.blog.models import Blog, Category
 
-admin.site.register(Blog)
+
+# Milestone 1, task 2
+class CustomBlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "enabled")
+    list_filter = ("enabled",)
+
+
+admin.site.register(Blog, CustomBlogAdmin)
 admin.site.register(Category)
